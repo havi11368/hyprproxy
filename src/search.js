dec08a0055df5ae03fc22c1be0607cc56ea19e31
Toast <85591146+havi11368@users.noreply.github.com>
2026-08-07 00:01:26 -0400

    add url check to the search page

--- src/search.js
+++ src/search.js
@@ -1,5 +1,12 @@
-document.getElementById("pxysearch").addEventListener("keydown", function(e) {
+document.querySelector("pxysearch").addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
+        if (document.querySelector("#pxysearch").value.includes("://")) {
+            window.location.href = sjEncode(document.querySelector("#pxysearch").value)
+          } else if (document.querySelector("#pxysearch").value.includes(".")) {
+            window.location.href = sjEncode("https://" + document.querySelector("#pxysearch").value)
+          } else {
+            window.location.href = sjEncode("https://" + "google.com" + "/search?q=" + document.querySelector("#urlBox").value)
+          }
         window.location.href = sjEncode(document.getElementById("pxysearch").value)
     }
 })

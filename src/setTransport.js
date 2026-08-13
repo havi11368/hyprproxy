92f101f3f05bb23d64b2cc7eb4110fe26f3bf948
Toast <85591146+havi11368@users.noreply.github.com>
2026-08-13 00:14:35 -0400

    add the scripts to html heads and hello controller thing again

--- src/setTransport.js
+++ src/setTransport.js
@@ -1,5 +1,15 @@
 //Taken from UV docs + poorly documented Scramjet docs
 
+const { ScramjetController } = $scramjetLoadController();
+const scramjet = new ScramjetController({
+	files: {
+		wasm: "/scram/scramjet.wasm",
+		all: "/scram/scramjet.js",
+    sync: "/scram/scramjet.sync.js",
+	},
+});
+scramjet.init();
+
 async function setTransport(transportsel) {
 
   const connection = new BareMux.BareMuxConnection("/baremux/worker.js")

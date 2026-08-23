aa8814460b9305cc0724d17cd8e0c7168cc5b368
Toast <85591146+havi11368@users.noreply.github.com>
2026-08-23 17:59:56 -0400

    pull from ACE lol

--- src/search.js
+++ src/search.js
@@ -1,11 +1,16 @@
-document.querySelector("#pxysearch").addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-        if (document.querySelector("#pxysearch").value.includes("://")) {
+function searchIt() {
+if (document.querySelector("#pxysearch").value.includes("://")) {
             window.location.href = sjEncode(document.querySelector("#pxysearch").value)
           } else if (document.querySelector("#pxysearch").value.includes(".")) {
             window.location.href = sjEncode("https://" + document.querySelector("#pxysearch").value)
           } else {
             window.location.href = sjEncode("https://" + "google.com" + "/search?q=" + document.querySelector("#urlBox").value)
           }
+        window.location.href = sjEncode(document.getElementById("pxysearch").value)
+}
+
+document.querySelector("#pxysearch").addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        searchIt()
     }
 })
